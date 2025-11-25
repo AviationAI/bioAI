310d6f55e869fc2c6bc84bce0da3d2ec33e2c0c1
snehin <snehindasgupta@gmail.com>
2025-11-24 21:15:56 -0800

    Added Project front/backend serializer

--- bioAI/api/serializers.py
+++ bioAI/api/serializers.py
@@ -6,15 +6,30 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        read_only_fields = ['id']
 
-class ProjectSerializer(serializers.ModelSerializer):
+class ProjectFrontendSerializer(serializers.ModelSerializer):
     summary = serializers.CharField(required=False, allow_blank=True)
     available_trusted_literatures = serializers.JSONField(required=False)
     objective = serializers.CharField(required = False)
-    editors = serializers.PrimaryKeyRelatedField(many = True, required=False, queryset = User.objects.all())
-    viewers = serializers.PrimaryKeyRelatedField(many = True, queryset = User.objects.all(), required=False)
-    
+    editors = UserSerializer(many = True, required=False)
+    viewers = UserSerializer(many = True, required=False)
+    user = UserSerializer()
+
     class Meta:
         model = Project
         fields = '__all__'
-        read_only_fields = ['id', 'user', 'AIsteps', 'editors', 'viewers']
+        read_only_fields = ['id', 'user']
+
+class ProjectBackendSerializer(serializers.ModelSerializer):
+    summary = serializers.CharField(required=False, allow_blank=True)
+    available_trusted_literatures = serializers.JSONField(required=False)
+    objective = serializers.CharField(required = False)
+    editors = serializers.PrimaryKeyRelatedField(many = True, queryset = User.objects.all(),required=False)
+    viewers = serializers.PrimaryKeyRelatedField(many = True, queryset = User.objects.all(), required=False)
+    user = UserSerializer()
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+        read_only_fields = ['id', 'user']
