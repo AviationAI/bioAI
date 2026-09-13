a1a2600be13e1c90c90d7109b517d42ca96dc7b6
AviationAI <snehindasgupta@gmail.com>
2026-09-12 19:45:27 -0700

    fixed bug in class names for editable field and text area

--- frontend/src/components/shared/editable_field.tsx
+++ frontend/src/components/shared/editable_field.tsx
@@ -3,7 +3,7 @@ import type React from "react";
 
 function EditableField({label, value, setValue, placeholder, sizeClass, fontClass}: {label?: string, value: string, setValue: React.Dispatch<SetStateAction<string>>, placeholder?: string, sizeClass?: string, fontClass?: string}) {
     
-    const classString = "px-3 py-2 w-full border rounded-md" + (sizeClass ?? "") + (fontClass ?? "");
+    const classString = "px-3 py-2 w-full border rounded-md " + " " + (sizeClass ?? "") + " " + (fontClass ?? "");
     
     return (
         <div className = "mb-3 w-full">
